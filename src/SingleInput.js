e469564f431658034342919fe6938485a3048144
hanamon <devparkhana@gmail.com>
2021-06-05 22:07:43 +0900

    [ADD] SingleInput Component

--- src/SingleInput.js
+++ src/SingleInput.js
@@ -14,7 +14,7 @@ function SingleInput() {
 
   return (
     <div className="container">
-      <h2>동적으로 input 값 출력하기</h2>
+      <h2>단일 input 상태 관리하기</h2>
       <div>
         <input type="text" value={value} onChange={setValueFn} />
         <button onClick={resetFn}>초기화</button>
